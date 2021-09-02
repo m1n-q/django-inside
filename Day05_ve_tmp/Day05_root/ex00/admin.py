9b70915cd798285de096974f9eb271f338756250
m1n-q <m1n_q@naver.com>
2021-09-02 18:22:31 +0900

    Day09 done

--- Day05_ve_tmp/Day05_root/ex00/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Driver, Car
-# Register your models here.
-
-admin.site.register(Driver)
-admin.site.register(Car)
-
--- Day05_ve_tmp/Day05_root/ex00/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'ex00'
-urlpatterns = [
-	path('init/', views.init, name='init')
-]
